cee9871e0dde892e000d05fcc7c7d2ce60ed1966
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-05-02 16:06:33 -0500

    took away input

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component }from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, ModalHeader, ModalBody, Modal, Label, Input } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, ModalHeader, ModalBody, Modal, Label} from 'reactstrap';
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
